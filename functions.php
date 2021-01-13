5fd896592fc5f50ac9a98b164fb79d88ae3f7b05
dumuch <dumuch@yandex.ru>
2021-01-13 16:20:50 +0800

    Добавлена функция db_get_prepare_stmt

--- functions.php
+++ functions.php
@@ -10,72 +10,107 @@ function render_template($__view, $__data)
 }
 
 
-function out_time() {
-  date_default_timezone_set('Asia/Irkutsk');
-  $dates=explode("-", date("m-d-Y"));
-  $then=mktime (0,0,0,$dates[0],$dates[1]+1,$dates[2]);
-  $now=time();
-  $how=$then-$now;
-  $hours=floor($how/3600);
-  $mins=floor(($how-($hours*3600))/60);
+function out_time()
+{
+    date_default_timezone_set('Asia/Irkutsk');
+    $dates=explode("-", date("m-d-Y"));
+    $then=mktime(0, 0, 0, $dates[0], $dates[1]+1, $dates[2]);
+    $now=time();
+    $how=$then-$now;
+    $hours=floor($how/3600);
+    $mins=floor(($how-($hours*3600))/60);
     return $hours . " : " . $mins;
 }
 
 
 $visit_name = 'visit_pages';
-function visit_history($lot_id, $visit_name) {
+function visit_history($lot_id, $visit_name)
+{
+    $visit_id = [];
+    $coincidence = false;
+    $expire = strtotime('+30 days');
+    $path = '/';
 
-	$visit_id = [];
-	$coincidence = false;
-	$expire = strtotime('+30 days');
-	$path = '/';
+    if (isset($_COOKIE[$visit_name])) {
+        $visit_id = json_decode($_COOKIE[$visit_name]);
 
-	if (isset($_COOKIE[$visit_name])) {
+        foreach ($visit_id as $key) {
+            if ($key == $lot_id) {
+                $coincidence = true;
+            }
+        }
+    };
 
-		$visit_id = json_decode($_COOKIE[$visit_name]);
+    if ($coincidence == false) {
+        array_push($visit_id, $lot_id);
+    };
 
-		foreach ($visit_id as $key) {
-			if ($key == $lot_id) {
-				$coincidence = true;
-			}
-		}
-	};
+    $visit_id = json_encode($visit_id);
 
-	if($coincidence == false) {
-		array_push($visit_id, $lot_id);
-	};
-
-	$visit_id = json_encode($visit_id);
-
-	setcookie($visit_name, $visit_id, $expire, $path);
+    setcookie($visit_name, $visit_id, $expire, $path);
 }
 
 
-function define_visit_history($visit_name, $products) {
+function define_visit_history($visit_name, $products)
+{
+    $visit_value = json_decode($_COOKIE[$visit_name]);
 
-	$visit_value = json_decode($_COOKIE[$visit_name]);
+    $products_history = [];
 
-	$products_history = [];
+    if (!empty($visit_value)) {
+        foreach ($visit_value as $item) {
+            foreach ($products as $key => $value) {
+                if ($item == $value['id']) {
+                    array_push($products_history, $products[$key]);
+                };
+            }
+        }
+    }
 
-	if(!empty($visit_value)) {
-		foreach ($visit_value as $item) {
-			foreach ($products as $key => $value) {
-				if ($item == $value['id']) {
-					array_push($products_history, $products[$key]);
-				};
-			}
-		}
-	}
-
-	return $products_history;
+    return $products_history;
 }
 
-function searchUserByEmail($email, $users) {
-	$user = [];
-	foreach ($users as $item) {
-		if($email == $item['email']) {
-			$user = $item;
-		}
-	}
-	return $user;
+function searchUserByEmail($email, $users)
+{
+    $user = [];
+    foreach ($users as $item) {
+        if ($email == $item['email']) {
+            $user = $item;
+        }
+    }
+    return $user;
+}
+
+
+function db_get_prepare_stmt(mysqli $link, string $sql, array $data = [])
+{
+    $stmt = mysqli_prepare($link, $sql);
+
+    if (empty($data)) {
+        return $stmt;
+    }
+
+    static $allowed_types = [
+        'integer' => 'i',
+        'double' => 'd',
+        'string' => 's',
+    ];
+
+    $types = '';
+    $stmt_data = [];
+
+    foreach ($data as $value) {
+        $type = gettype($value);
+
+        if (!isset($allowed_types[$type])) {
+            throw new \UnexpectedValueException(sprintf('Unexpected parameter type "%s".', $type));
+        }
+
+        $types .= $allowed_types[$type];
+        $stmt_data[] = $value;
+    }
+
+    mysqli_stmt_bind_param($stmt, $types, ...$stmt_data);
+
+    return $stmt;
 }
